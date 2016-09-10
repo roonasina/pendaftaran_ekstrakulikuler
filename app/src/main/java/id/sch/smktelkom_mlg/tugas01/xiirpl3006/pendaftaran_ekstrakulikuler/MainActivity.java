2305b4e14d1912e9795c8b367f77c02940d32047
SMK Telkom SP Malang <annisanoor71@gmail.com>
2016-09-10 13:18:49 +0700

    menampilkan output dari edit text

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl3006/pendaftaran_ekstrakulikuler/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl3006/pendaftaran_ekstrakulikuler/MainActivity.java
@@ -2,12 +2,34 @@ package id.sch.smktelkom_mlg.tugas01.xiirpl3006.pendaftaran_ekstrakulikuler;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    EditText ednama;
+    TextView tvhasil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ednama = (EditText) findViewById(R.id.editTextnama);
+        tvhasil = (TextView) findViewById(R.id.textViewhasil);
+        findViewById(R.id.buttonok).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                doClick();
+
+            }
+        }
+        );
+    }
+
+    private void doClick() {
+        String nama = ednama.getText().toString();
+        StringBuilder builder = new StringBuilder();
+        builder.append("Nama : " + nama);
+        tvhasil.setText(builder);
     }
 }
