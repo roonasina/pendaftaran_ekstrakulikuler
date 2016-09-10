1a404c2c67c2f2e6e4b85c4a850800b1a4b2af91
SMK Telkom SP Malang <annisanoor71@gmail.com>
2016-09-10 13:04:02 +0700

    membuat tampilan form

---
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl3006/pendaftaran_ekstrakulikuler/MainActivity.java
@@ -0,0 +1,13 @@
+package id.sch.smktelkom_mlg.tugas01.xiirpl3006.pendaftaran_ekstrakulikuler;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
