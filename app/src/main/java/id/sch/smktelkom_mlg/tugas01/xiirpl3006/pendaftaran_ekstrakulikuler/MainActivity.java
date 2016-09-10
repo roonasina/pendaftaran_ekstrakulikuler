9a3c0fa43693e4ddbe36bb8e8fce4b973b4568b2
SMK Telkom SP Malang <annisanoor71@gmail.com>
2016-09-10 15:18:15 +0700

    menampilkan validasi dan mengganti warna bacground

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl3006/pendaftaran_ekstrakulikuler/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl3006/pendaftaran_ekstrakulikuler/MainActivity.java
@@ -31,45 +31,63 @@ public class MainActivity extends AppCompatActivity {
         cbtf = (CheckBox) findViewById(R.id.checkBoxtfsr);
         tvhasil = (TextView) findViewById(R.id.textViewhasil);
         findViewById(R.id.buttonok).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                doClick();
+                                                           @Override
+                                                           public void onClick(View view) {
+                                                               doClick();
 
-            }
-        }
+                                                           }
+                                                       }
         );
     }
 
     private void doClick() {
+        if (isValid()) {
+            String nama = ednama.getText().toString();
+            StringBuilder builder = new StringBuilder();
+            String hasil = null;
+            if (rgjk.getCheckedRadioButtonId() != -1) {
+                RadioButton rb = (RadioButton) findViewById(rgjk.getCheckedRadioButtonId());
+                hasil = rb.getText().toString();
+            }
+            if (hasil == null) {
+                tvhasil.setText("Anda belum mengisi jenis kelamin");
+            } else {
+                String ekskul = "\t Ekstrakulikuler yang Dipilih \t\n";
+                int startlen = ekskul.length();
+                if (cbbsk.isChecked()) ekskul += "\t\t - " + cbbsk.getText() + "\n";
+                if (cbps.isChecked()) ekskul += "\t\t - " + cbps.getText() + "\n";
+                if (cbfo.isChecked()) ekskul += "\t\t - " + cbfo.getText() + "\n";
+                if (cbpr.isChecked()) ekskul += "\t\t - " + cbpr.getText() + "\n";
+                if (cbtf.isChecked()) ekskul += "\t\t - " + cbtf.getText() + "\n";
+                if (ekskul.length() == startlen) {
+                    tvhasil.setText("Anda belum memilih ekskul");
+                } else {
+                    builder.append("Nama : " + nama + "\n");
+                    builder.append("Kelas : ");
+                    builder.append(spkelas.getSelectedItem().toString() + "\n");
+                    builder.append("Jenis kelamin : " + hasil + "\n");
+                    builder.append(ekskul + "\n");
+                    tvhasil.setText(builder);
+                }
+            }
+        }
+    }
+
+
+    private boolean isValid() {
+        boolean valid = true;
+
         String nama = ednama.getText().toString();
-        StringBuilder builder = new StringBuilder();
-        String hasil = null;
-        if (rgjk.getCheckedRadioButtonId() != -1){
-            RadioButton rb = (RadioButton) findViewById(rgjk.getCheckedRadioButtonId());
-            hasil = rb.getText().toString();
+
+        if (nama.isEmpty()) {
+            ednama.setError("Nama belum diisi");
+            valid = false;
+        } else if (nama.length() < 3) {
+            ednama.setError("Nama minimal 3 karakter");
+            valid = false;
+        } else {
+            ednama.setError(null);
         }
-        if (hasil==null){
-            tvhasil.setText("Anda belum mengisi jenis kelamin");
-        }
-        else {
-            String ekskul = "\t Ekstrakulikuler yang Dipilih \t\n";
-            int startlen = ekskul.length();
-            if (cbbsk.isChecked()) ekskul += "\t\t - " +cbbsk.getText() + "\n";
-            if (cbps.isChecked()) ekskul += "\t\t - " +cbps.getText() + "\n";
-            if (cbfo.isChecked()) ekskul += "\t\t - " +cbfo.getText() + "\n";
-            if (cbpr.isChecked()) ekskul += "\t\t - " +cbpr.getText() + "\n";
-            if (cbtf.isChecked()) ekskul += "\t\t - " +cbtf.getText() + "\n";
-            if (ekskul.length() == startlen) {
-                tvhasil.setText("Anda belum memilih ekskul");
-            }
-            else {
-                builder.append("Nama : " + nama + "\n");
-                builder.append("Kelas : ");
-                builder.append(spkelas.getSelectedItem().toString() + "\n");
-                builder.append("Jenis kelamin : " + hasil + "\n");
-                builder.append(ekskul + "\n");
-                tvhasil.setText(builder);
-            }
-        }
+        return  valid;
     }
 }
